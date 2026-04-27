54ff260248ede39acb27e8e1a6fe0fd2afb8cddc
abdullahi mohamed <abdullahi.mohamed2324@gmail.com>
2026-04-27 14:13:16 -0500

    starter for void post service

---
+++ void-post/src/main/java/com/abdulmo123/void_post/VoidPostApplication.java
@@ -0,0 +1,13 @@
+package com.abdulmo123.void_post;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoidPostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoidPostApplication.class, args);
+	}
+
+}
---
+++ void-post/src/test/java/com/abdulmo123/void_post/VoidPostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdulmo123.void_post;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoidPostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
