bee8194e8f144efa0e4d0c65203ece1199ee10dd
abdullahi mohamed <abdullahi.mohamed2324@gmail.com>
2026-04-27 14:12:47 -0500

    starter for void user service

---
+++ void-user/src/main/java/com/abdulmo123/void_user/VoidUserApplication.java
@@ -0,0 +1,13 @@
+package com.abdulmo123.void_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoidUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoidUserApplication.class, args);
+	}
+
+}
---
+++ void-user/src/test/java/com/abdulmo123/void_user/VoidUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdulmo123.void_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoidUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
