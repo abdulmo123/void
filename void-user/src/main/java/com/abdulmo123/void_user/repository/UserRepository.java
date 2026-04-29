2d85352e7faa0ca824f1bbdbff5ed6d9bd8d52dc
abdullahi mohamed <abdullahi.mohamed2324@gmail.com>
2026-04-29 17:32:04 -0500

    allow for find by username/email

--- void-user/src/main/java/com/abdulmo123/void_user/repository/UserRepository.java
+++ void-user/src/main/java/com/abdulmo123/void_user/repository/UserRepository.java
@@ -2,11 +2,19 @@ package com.abdulmo123.void_user.repository;
 
 import com.abdulmo123.void_user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email);
+
+    @Query(value = """
+                select * from usr.users u 
+                where u.email = :input or u.username = :input
+            """, nativeQuery = true
+    )
+    Optional<User> findByUsernameOrEmail(@Param("input") String input);
 }
