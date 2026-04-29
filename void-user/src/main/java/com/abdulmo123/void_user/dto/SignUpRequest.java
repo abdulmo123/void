5887cc2e724f64a64ee61a91a25fc548ab85b0c0
abdullahi mohamed <abdullahi.mohamed2324@gmail.com>
2026-04-29 17:32:14 -0500

    signup request dto

---
+++ void-user/src/main/java/com/abdulmo123/void_user/dto/SignUpRequest.java
@@ -0,0 +1,18 @@
+package com.abdulmo123.void_user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class SignUpRequest {
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String email;
+    private String password;
+}
