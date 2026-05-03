54a437cfe619d1716883158c583774539b5ae824
abdullahi mohamed <abdullahi.mohamed2324@gmail.com>
2026-05-02 19:55:41 -0500

    update swagger for void-user service to include bearer token usage

--- void-user/src/main/java/com/abdulmo123/void_user/config/SwaggerConfig.java
+++ void-user/src/main/java/com/abdulmo123/void_user/config/SwaggerConfig.java
@@ -1,7 +1,10 @@
 package com.abdulmo123.void_user.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,6 +16,12 @@ public class SwaggerConfig {
                 .info(new Info()
                         .title("Void User Service API")
                         .description("Handles authentication and user management")
-                        .version("1.0.0"));
+                        .version("1.0.0"))
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+                .components(new Components()
+                        .addSecuritySchemes("Bearer Authentication", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
     }
 }
